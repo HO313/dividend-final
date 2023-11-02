7e2fce2d79e1d29700791b03b74186b76dbe12ac
HO313 <wonik15@gmail.com>
2023-11-02 22:30:41 +0900

    dividend v0.01 start project

---
+++ src/main/java/zerobase/dividendfinal/DividendFinalApplication.java
@@ -0,0 +1,13 @@
+package zerobase.dividendfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendFinalApplication.class, args);
+    }
+
+}
